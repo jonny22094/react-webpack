107beb427da16ec844405053de879459785185ec
Jan Rochalski <qwsazxxzaswq@wp.pl>
2019-03-04 20:17:42 +0100

    chore(): render types

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,5 +1,13 @@
+//@flow
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '@app/app'
 
-ReactDOM.render(<App/>, document.getElementById('app'))
+const root = document.getElementById('app')
+
+if (root !== null) {
+  ReactDOM.render(<App/>, root)
+} else {
+  throw new Error('Missing root element')
+}
+
