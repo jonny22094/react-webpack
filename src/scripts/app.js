8cca342aa23aa4429f0a724ec5bd457a8871419f
Jan Rochalski <qwsazxxzaswq@wp.pl>
2019-03-04 21:14:40 +0100

    feat(): ThemeProvider

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,17 +1,21 @@
 //@flow
 import React, {Component} from 'react'
+import {ThemeProvider} from 'styled-components'
 import {GlobalStyles} from '@app/components/globalStyles'
 import {Container, Image} from '@app/components/styles'
+import {theme} from '@app/config'
 import Logo from '@app/images/logo.png'
 
 class App extends Component<{}> {
   render() {
     return (
-      <Container>
-        <GlobalStyles/>
-        <Image src={Logo} width={'150px'}/>
-        <p>Hello world!</p>
-      </Container>
+      <ThemeProvider theme={theme}>
+        <Container>
+          <GlobalStyles/>
+          <Image src={Logo} width={'150px'}/>
+          <p>Hello world!</p>
+        </Container>
+      </ThemeProvider>
     )
   }
 }
