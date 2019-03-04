c1e1b1da44044ffd69ac2d1705d6d27a995798ef
Jan Rochalski <qwsazxxzaswq@wp.pl>
2019-03-04 18:01:30 +0100

    chore(): add flow status command

--- src/scripts/components/globalStyles.js
+++ src/scripts/components/globalStyles.js
@@ -3,6 +3,13 @@ import {createGlobalStyle} from 'styled-components'
 
 export const GlobalStyles = createGlobalStyle`
   body {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 100vw;
+    height: 100vh;
+
     margin: 0;
   }
 `
